08389f8cf4352a36dd10c8f5f42ccee3c6dcae1f
Liwenxi <lwx5201314@>
2019-12-09 11:19:45 +0800

    XX

---
+++ src/main/java/com/third/pojo/Cart.java
@@ -0,0 +1,43 @@
+package com.third.pojo;
+
+public class Cart {
+    private Integer id;
+
+    private Integer pid;
+
+    private Integer count;
+
+    private Double sum;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Double getSum() {
+        return sum;
+    }
+
+    public void setSum(Double sum) {
+        this.sum = sum;
+    }
+}
